ca61ac178a608530a4ac038ca69cdcc7c7ca6677
Sir Eric <echeungsk@gmail.com>
2020-03-29 00:36:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,10 +37,8 @@ function switchOnCharmFromTip(tip){
   switch (tip) {
     case 'generous':
       return 'Thank you so much.';
-      break;
     case 'not as generous':
       return 'Thank you.';
-      break;
     default:
       return 'Bye';
   }
