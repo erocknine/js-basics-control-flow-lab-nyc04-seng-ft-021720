1181c51339190cabcb18b69f06f710d8fad32514
Sir Eric <echeungsk@gmail.com>
2020-03-29 00:24:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 function scuberGreetingForFeet(feet){
   if (feet > 2000) return 'I will gladly take your thirty bucks.';
   else if (feet <= 400) return "This one is on me!";
-  else (feet > 2500) return 'No can do.';
+  else if (feet > 2500) return 'No can do.';
 }
 
-let value
-  if (feet >= 2000) {
-    value = 'I will gladly take your thirty bucks.';
-    return value
-  } else if (feet <= 400) {
-    value = "This one is on me!";
-    return value
-  } else if (feet > 2500) {
-    value = 'No can do.';
-    return value
-  }
-function scuberGreetingForFeet(feet){
-  switch (feet) {
-    case feet > 2500
-      return 'No can do.';
-      break;
-    case feet > 2000
-      return 'I will gladly take your thirty bucks.';
-      break;
-    case feet <= 400
-      return 'This one is on me!';
-      break;
-  }
-}
+// let value
+//   if (feet >= 2000) {
+//     value = 'I will gladly take your thirty bucks.';
+//     return value
+//   } else if (feet <= 400) {
+//     value = "This one is on me!";
+//     return value
+//   } else if (feet > 2500) {
+//     value = 'No can do.';
+//     return value
+//   }
+// function scuberGreetingForFeet(feet){
+//   switch (feet) {
+//     case feet > 2500
+//       return 'No can do.';
+//       break;
+//     case feet > 2000
+//       return 'I will gladly take your thirty bucks.';
+//       break;
+//     case feet <= 400
+//       return 'This one is on me!';
+//       break;
+//   }
+// }
 
 function ternaryCheckCity(){
   // Write your code here!
