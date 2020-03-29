824c4e6f80ab84aea5b48b3f8b3d54016da6d073
Sir Eric <echeungsk@gmail.com>
2020-03-29 00:36:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function scuberGreetingForFeet(feet){
 // }
 
 function ternaryCheckCity(city){
-  if city === 'NYC' ? "Ok, sounds good." : "No go.";
+  if city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
 function switchOnCharmFromTip(tip){
