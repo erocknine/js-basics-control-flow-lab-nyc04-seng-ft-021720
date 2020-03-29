69318290f23b36ebce605f87abc7e6b9f7912e06
Sir Eric <echeungsk@gmail.com>
2020-03-29 00:27:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function scuberGreetingForFeet(feet){
-  if (2500 > feet > 2000) return 'I will gladly take your thirty bucks.';
+  if (feet > 2000 && feet < 2500) return 'I will gladly take your thirty bucks.';
   else if (feet <= 400) return "This one is on me!";
   else if (feet > 2500) return 'No can do.';
 }
